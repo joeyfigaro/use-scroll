074b3f77cab5975278f3756613ff8969e866a0a0
Jay Figaro <joey@joeyfigaro.com>
2024-12-01 17:38:17 -0500

    docs: configure CHANGELOG; update envrc example [skip-ci]

--- release.config.mjs
+++ release.config.mjs
@@ -2,10 +2,13 @@ export default {
   branches: [
     '+([0-9])?(.{+([0-9]),x}).x',
     'main',
-    'next',
-    { name: 'beta', prerelease: true },
+    { name: 'beta', channel: 'beta', prerelease: true },
   ],
   plugins: [
+    {
+      path: '@semantic-release/changelog',
+      changelogFile: 'CHANGELOG.md',
+    },
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/npm',
